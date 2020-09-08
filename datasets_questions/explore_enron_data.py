918fd2153bf495545c21d0e8f73c170f64441184
Satyajit Ovelil <satyajitovelil@gmail.com>
2020-09-08 21:53:26 +0530

    lesson 6

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -25,3 +25,32 @@ print len(enron_data[enron_data.keys()[0]].keys())
 pois = [k for k,v in enron_data.items() if v["poi"] == True]
 
 print len(pois)
+
+# print enron_data["PRENTICE JAMES"]['total_stock_value']
+
+# print enron_data["COLWELL WESLEY"]['from_this_person_to_poi']
+
+# print enron_data["SKILLING JEFFREY K"]['exercised_stock_options']
+
+# print enron_data["LAY KENNETH L"]['total_payments']
+# print enron_data["SKILLING JEFFREY K"]['total_payments']
+# print enron_data["FASTOW ANDREW S"]['total_payments']
+
+salaries = [k for k,v in enron_data.items() if v["salary"] != 'NaN']
+print len(salaries)
+
+email_addresses = [k for k,v in enron_data.items() if v["email_address"] != 'NaN']
+print len(email_addresses)
+
+total_payments_nan = [k for k,v in enron_data.items() if v["total_payments"] == 'NaN']
+
+poi_total_payments = []
+for k,v in enron_data.items():
+    if v["poi"] == True:
+        if v["total_payments"] == 'NaN':
+            poi_total_payments.append(k)
+
+
+print len(total_payments_nan)
+print len(poi_total_payments)
+print len(enron_data.keys())
